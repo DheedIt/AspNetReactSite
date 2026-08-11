4a25d1ea06adf197e4e6e2a177c1a04582b9a12c
DheedIt <zhulanoff.ilia@yandex.ru>
2026-08-11 21:45:43 +0400

    codegeneratohui

--- Back/ClockSite/obj/Debug/net10.0/ClockSite.AssemblyInfo.cs
+++ Back/ClockSite/obj/Debug/net10.0/ClockSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClockSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70c7b81cac8ecee6a7c9f0d6cb8d1ee94c4f7c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93184e098454d72c37f2b4628320f061d20f3b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClockSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClockSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
