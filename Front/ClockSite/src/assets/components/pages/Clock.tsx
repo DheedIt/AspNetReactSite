189d10945c5e49f5598089f00411504a15a2df28
DheedIt <zhulanoff.ilia@yandex.ru>
2026-08-11 22:42:31 +0400

    Макет новой страницы

---
+++ Front/ClockSite/src/assets/components/pages/Clock.tsx
@@ -0,0 +1,9 @@
+
+export const Clock = () => {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', width: '100%' }}>
+      <h2>⏰ Это отдельная страница часов!</h2>
+      <p>Старый контент Vite здесь отображаться не должен.</p>
+    </div>
+  )
+}
